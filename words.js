7df2b35a6debbb5075968c0b7614e4fa6608d818
nicolemeese <sudamericanartchic@gmail.com>
2025-04-18 15:49:41 -0500

    Create words.js

---
+++ words.js
@@ -0,0 +1,13 @@
+// Korean-English word pairs + Google TTS links
+const words = [
+  {
+    english: "Korean restaurant",
+    korean: "한식당",
+    audio: "https://translate.google.com/translate_tts?ie=UTF-8&tl=ko&q=한식당&client=tw-ob"
+  },
+  {
+    english: "Hello",
+    korean: "안녕하세요",
+    audio: "https://translate.google.com/translate_tts?ie=UTF-8&tl=ko&q=안녕하세요&client=tw-ob"
+  }
+];
