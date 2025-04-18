e9468591fe066e18022943b53077e80f7752a082
nicolemeese <sudamericanartchic@gmail.com>
2025-04-18 15:54:21 -0500

    words.js

--- words.js
+++ words.js
@@ -1,13 +1,15 @@
 // Korean-English word pairs + Google TTS links
-const words = [
-  {
-    english: "Korean restaurant",
-    korean: "한식당",
-    audio: "https://translate.google.com/translate_tts?ie=UTF-8&tl=ko&q=한식당&client=tw-ob"
-  },
-  {
-    english: "Hello",
-    korean: "안녕하세요",
-    audio: "https://translate.google.com/translate_tts?ie=UTF-8&tl=ko&q=안녕하세요&client=tw-ob"
-  }
-];
+const wordCategories = {
+  "TOPIK I": [
+    { english: "Hello", korean: "안녕하세요", audio: "https://...안녕하세요&client=tw-ob" },
+    { english: "Thank you", korean: "감사합니다", audio: "https://...감사합니다&client=tw-ob" }
+  ],
+  "TOPIK II": [
+    { english: "Economy", korean: "경제", audio: "https://...경제&client=tw-ob" },
+    { english: "Discussion", korean: "토론", audio: "https://...토론&client=tw-ob" }
+  ],
+  "Verbs": [
+    { english: "To eat", korean: "먹다", audio: "https://...먹다&client=tw-ob" },
+    { english: "To go", korean: "가다", audio: "https://...가다&client=tw-ob" }
+  ]
+};
